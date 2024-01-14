43dfc389d69476e237589d089b7eb47b9f48628d
Srihari7095 <114302327+Srihari7095@users.noreply.github.com>
2024-01-14 13:16:44 +0530

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 //https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './', // Publish directory is the current directory
+  },
   plugins: [react()],
 })
